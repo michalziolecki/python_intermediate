d809c6d2d06c8580e33dad9061959908d2bd0923
michal.ziolecki <michal.ziolecki@op.pl>
2020-07-29 22:36:51 +0200

    ex 7

--- sda_exercises_oop_1_mz/ex1_10/dog.py
+++ sda_exercises_oop_1_mz/ex1_10/dog.py
@@ -1,7 +1,9 @@
+from sda_exercises_oop_1_mz.canidae import Canidae
 from sda_exercises_oop_1_mz.ex1_10.animal import Animal
+from sda_exercises_oop_1_mz.mammal import Mammal
 
 
-class Dog(Animal):
+class Dog(Animal, Mammal, Canidae):
 
     def __init__(self, name):
         super().__init__(name)  # jesli dziedziczymy i klasa Ojciec ma to pole to przekazujemy przez super
