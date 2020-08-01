c31633f58526097af6bae1da361c44b183e72060
michal.ziolecki <michal.ziolecki@op.pl>
2020-08-01 22:16:51 +0200

    Order exercises added

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sda_exercises_oop_1_mz.ex1_10.dog import Dog
 
 # zwrocenie wartosci przez przypisanie - return
 from sda_exercises_oop_3_mz.complex import MyComplex
+from sda_exercises_oop_3_mz.order import Order
+from sda_exercises_oop_3_mz.order_item import *
 
 
 def ex2() -> list:
@@ -60,29 +62,51 @@ def main():
     # employee.who_am_i()
 
     # 3_mz
-    number1 = MyComplex(100, 20)
-    number1.show()
-    print(number1)
+    # number1 = MyComplex(100, 20)
+    # number1.show()
+    # print(number1)
+    #
+    # number2 = MyComplex(100, 20)
+    # number2.show()
+    # print(number2)
+    #
+    # number3 = MyComplex(101, 20)
+    # number3.show()
+    # print(number3)
+    #
+    # print(f'compare complex: {number2.is_equal_to(100, 20)}')
+    # print(f'compare complex: {number1==number2}')
+    # print(f'compare complex: {number2==number3}')
+    #
+    # complex_list = [number1, number2, number3]
+    #
+    # print(MyComplex.add_two_complex(number1, number2))
+    # print(MyComplex.add_three_complex(number1, number2, number3))
+    # print(MyComplex.add_many_complex(complex_list))
+    #
+    # print(number1.add(number2))
+    item1 = OrderItem('Ser', 3, Decimal(4.50))
+    item1.show()
+    print(item1)
+    print(item1.is_correct())
 
-    number2 = MyComplex(100, 20)
-    number2.show()
-    print(number2)
+    item2 = OrderItem('Masło', 0, Decimal(6.50))
+    item2.show()
+    print(item2)
+    try:
+        print(item2.get_value())
+    except ValueError as ve:
+        print(f'Error cached {ve.args}')
+    print(item2.is_correct())
 
-    number3 = MyComplex(101, 20)
-    number3.show()
-    print(number3)
+    item3 = OrderItem('Musztarda', 2, Decimal(2.50))
+    item3.show()
 
-    print(f'compare complex: {number2.is_equal_to(100, 20)}')
-    print(f'compare complex: {number1==number2}')
-    print(f'compare complex: {number2==number3}')
+    order = Order()
+    order.add_item(item1)
+    order.add_item(item3)
+    order.show()
 
-    complex_list = [number1, number2, number3]
-
-    print(MyComplex.add_two_complex(number1, number2))
-    print(MyComplex.add_three_complex(number1, number2, number3))
-    print(MyComplex.add_many_complex(complex_list))
-
-    print(number1.add(number2))
 
 
 if __name__ == "__main__":
---
+++ sda_exercises_oop_3_mz/order_item.py
@@ -0,0 +1,26 @@
+from decimal import Decimal  # jest to typ o duzej i ustalonej precyzji dla walut
+
+
+class OrderItem:
+
+    def __init__(self, name: str, number: int, price: Decimal):
+        self.name = name
+        self.number = number
+        self.price = price
+        self.all_price: Decimal = self.number * self.price
+
+    def get_value(self) -> Decimal:
+        if not self.is_correct():
+            raise ValueError('Price for order equal or below zero!')
+        return self.all_price
+
+    def is_correct(self) -> bool:
+        if self.all_price <= Decimal(0):
+            return False
+        return True
+
+    def show(self) -> None:
+        print(f'{self.name} {self.price} {self.number} szt {self.price} zł')
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.price} {self.number} szt {self.price} zł'
